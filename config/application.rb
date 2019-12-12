7007eab59b5d92d46ba5ab92c1543580a452ffb4
devnok <ha819ha.dev@gmail.com>
2019-12-12 17:25:22 +0900

    initial commit

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Starter
+module NewsSite
   class Application < Rails::Application
     # Expose our application's helpers to Administrate
     config.to_prepare do
-      Administrate::ApplicationController.helper Starter::Application.helpers
+      Administrate::ApplicationController.helper NewsSite::Application.helpers
     end
     config.active_job.queue_adapter = :sidekiq
     config.application_name = Rails.application.class.module_parent_name
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(first_name: "seonung", last_name: "ha", email: "ha819ha.dev@gmail.com", password: "itis1234", password_confirmation: "itis1234", admin: true)
